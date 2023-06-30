ba7a611a50fbc598b413fddd8d20bfe217d35ae4
kimjunsung04 <phservice.kr@gmail.com>
2023-06-30 23:25:05 +0900

    ✨ 28235 풀이 추가

---
+++ 01 Bronze5/28235.py
@@ -0,0 +1,9 @@
+chant = input()
+if chant == "SONGDO":
+    print("HIGHSCHOOL")
+elif chant == "CODE":
+    print("MASTER")
+elif chant == "2023":
+    print("0611")
+elif chant == "ALGORITHM":
+    print("CONTEST")
