6664feea085d1ebe6f81b90578a0b6b8d2c6b5d7
kimjunsung04 <phservice.kr@gmail.com>
2023-07-10 00:58:57 +0900

    ✨ 5575 풀이 추가

---
+++ 02 Bronze4/5575.py
@@ -0,0 +1,7 @@
+for _ in range(3):
+    h1, m1, s1, h2, m2, s2 = map(int, input().split())
+    t = (h2*60*60 + m2*60 + s2) - (h1*60*60 + m1*60 + s1)
+    h = t//60//60 % 24
+    m = t//60 % 60
+    s = t % 60
+    print(h, m, s)
