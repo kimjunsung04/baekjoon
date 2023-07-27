031fd7040ffb4c560a36369e233975790ffde0da
kimjunsung04 <phservice.kr@gmail.com>
2023-07-27 21:01:19 +0900

    ✨ 16199 풀이 추가

---
+++ 02 Bronze4/16199.py
@@ -0,0 +1,6 @@
+y1, m1, d1 = map(int, input().split())
+y2, m2, d2 = map(int, input().split())
+if m2 > m1 or (m2 == m1 and d2 >= d1): print(y2-y1)
+else: print(y2-y1-1)
+print(y2-y1+1)
+print(y2-y1)
